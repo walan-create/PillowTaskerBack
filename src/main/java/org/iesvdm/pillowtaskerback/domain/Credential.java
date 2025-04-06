737f6520147fcee5d849da48d59979b68439a66b
walan-create <116348714+walan-create@users.noreply.github.com>
2025-04-06 20:39:18 +0200

    Parte intermedia - (Cambio entre empleado y credencial pendiente)

---
+++ src/main/java/org/iesvdm/pillowtaskerback/domain/Credential.java
@@ -0,0 +1,21 @@
+package org.iesvdm.pillowtaskerback.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Credential {
+
+    @Id
+    private Long id;
+
+
+}
